092a6d4e2607ef533aeeeeb97bcfc66155cb7361
Junhwan Choi <125186303+Jvvhw@users.noreply.github.com>
2023-03-06 18:14:52 +0900

    Update convert_to_floats.py

--- convert_to_floats.py
+++ convert_to_floats.py
@@ -9,3 +9,8 @@ def convert_to_floats(matrix):
             matrix[i][j] = float(matrix[i][j])
     return matrix
 
+'''
+위 코드에서 matrix는 실수로 변환하고자 하는 2차원 리스트를 의미합니다. 
+convert_to_floats 함수는 matrix를 인자로 받아, 2중 for 루프를 통해 matrix의 각 원소를 float 함수를 이용하여 실수로 변환한 뒤, 그 값을 다시 matrix에 저장합니다.
+마지막으로 변환된 matrix를 반환합니다.
+'''
